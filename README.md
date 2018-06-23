337d4da4f43661514effd71e382a80b124a3c5fc
ivankmk <kumeyko@hotmail.com>
2018-06-23 22:27:02 +0200

    script create

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+The script collecting data about 20 random courses from Coursera.org and save it as excel file.
+
+# How to launch?
+
+Before, install all dependencies:
+- ```bash pip install -r requirements.txt```
+
+Launch the script:
+- ```python3 coursera.py file.xlsx```
 
 # Project Goals
 
--- coursera.py
+++ coursera.py
@@ -1,16 +1,79 @@
+import urllib.request
+import xml.etree.ElementTree as ET
+import random
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import argparse
 
 
-def get_courses_list():
-    pass
+def get_courses_list(number_to_select=20):
+    url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    tree = ET.fromstring(urllib.request.urlopen(url).read())
+    schema = './/{http://www.sitemaps.org/schemas/sitemap/0.9}loc'
+    courses = tree.findall(schema)
+    return random.sample([course.text for course in courses], number_to_select)
 
 
 def get_course_info(course_slug):
-    pass
+    tree = urllib.request.urlopen(course_slug).read()
+    soup = BeautifulSoup(tree, 'lxml')
+    try:
+        course_lang = soup.find('div', attrs={'class':
+                                              'rc-Language'}).get_text()
+    except AttributeError:
+        course_lang = 'No data'
+    try:
+        start_date = soup.find('div', attrs={'id':
+                                             'start-date-string'}).get_text()
+    except AttributeError:
+        start_date = 'No data'
+    try:
+        cnt_weeks = len(soup.find_all('div', attrs={'class':
+                                                    'week'}))
+    except AttributeError:
+        cnt_weeks = 'No data'
+    try:
+        rating = soup.find('div', class_='ratings-text').text
+    except AttributeError:
+        rating = 'No data'
+    try:
+        course_name = soup.title.string.split('|')[0]
+    except AttributeError:
+        course_name = 'No data'
+
+    return {
+            'Course name': course_name,
+            'Course Language': course_lang,
+            'Start Date': start_date,
+            'Weeks lenght': cnt_weeks,
+            'Rating': rating
+            }
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def output_courses_info_to_xlsx(courses, filepath):
+    wb = Workbook()
+    ws = wb.active
+    ws.title = 'Coursera data'
+    ws.append(['Course name',
+               'Course Language',
+               'Start Date',
+               'Weeks lenght',
+               'Rating'])
+    for course in courses:
+        ws.append([course['Course name'],
+                   course['Course Language'],
+                   course['Start Date'],
+                   course['Weeks lenght'],
+                   course['Rating']])
+    wb.save(filename=filepath)
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(
+            description='Script will collect data from Cursera'
+            )
+    parser.add_argument('filepath', help='Please enter file in xlsx format')
+    args = parser.parse_args()
+    courses_list = get_courses_list()
+    course_info = [get_course_info(course) for course in courses_list]
+    output_courses_info_to_xlsx(course_info, args.filepath)
